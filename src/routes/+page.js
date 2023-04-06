d25264f08493029180a04150dd098bc3d5da7009
Liam <liamecahill@gmail.com>
2023-04-05 20:06:07 -0400

    added new mainpage styling. currently rendering.

---
+++ src/routes/+page.js
@@ -0,0 +1,11 @@
+// export const load = async () => {
+
+//     const getUsers = async () => {
+//         const res = await fetch("https://cfvxwwhu9k.execute-api.us-east-1.amazonaws.com/")
+//         const data = await res.json();
+//         return data
+//     };
+//     return {
+//         users: await getUsers(),
+//     };
+// };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,136 @@
 <script>
-    import Nav from "./Nav.svelte"
-
+    export let data;
+    const { users } = data;
+    console.log(users);
 </script>
 
+<body>
+    <div class="hero">
+        <img
+            class="hero-image"
+            src="/SOPPY_BOBS_dungeon_and_dragons_themed_underground_dungeon_priso_721f4a3b-ab56-4f5c-b52b-9d363c50b32c.png"
+            alt="dungeon"
+        />
+        <h1 class="hero-text">Dark-And-Darker-Marketplace</h1>
+    </div>
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            perspiciatis numquam animi possimus. Culpa, ducimus! Ullam, odio
+            laudantium culpa rem doloremque rerum voluptates? Libero eveniet
+            neque animi provident quis maiores. Nam expedita cum aperiam
+            exercitationem iure, veniam voluptatum architecto ipsa ea
+            consectetur praesentium, placeat, cupiditate omnis? Eaque
+            perferendis itaque quo voluptatibus repudiandae, vero deleniti,
+            perspiciatis dicta corrupti libero fugit. Soluta. Voluptates atque
+            accusantium sit quo necessitatibus architecto illum maxime sapiente!
+            Tempore, quia consectetur repellat architecto eius, maiores eum in,
+            illo ea est blanditiis at inventore facilis obcaecati dolore unde!
+            Reprehenderit. Sed quis quas nisi quae quam fugit? Maxime quibusdam
+            placeat corporis, ducimus, nam ut dolore ea delectus quae veniam
+            porro quaerat officiis? Consequatur quam neque quae, minima
+            praesentium sed perferendis. Eveniet quisquam voluptatum quod, eos
+            incidunt possimus unde consequuntur veritatis soluta, nobis
+            doloribus? Excepturi quod nam molestias. Deleniti, esse cupiditate
+            odit itaque, voluptate unde, vitae ex fugit eveniet rerum accusamus.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
+            natus modi quae aliquid adipisci dolorum culpa repellendus cum. Ea
+            nobis a odio corporis nostrum itaque ipsa quisquam eum amet
+            sapiente! Vitae magnam temporibus dolorem unde rerum ipsum
+            consequuntur nobis sint mollitia doloribus odit perferendis at, quia
+            harum esse, voluptatibus, nemo qui aperiam dolore expedita fuga
+            distinctio natus. Neque, totam ratione. Eius ab dolores, iusto ipsum
+            possimus eos quidem natus ea eum asperiores tempore dolorum
+            provident beatae minus dolore quos fugit mollitia ipsam eveniet fuga
+            similique debitis pariatur odio! Atque, nemo? Obcaecati dolor rerum
+            sit officiis, ducimus nostrum! Minima quae similique voluptates
+            voluptatibus deleniti sequi laborum doloremque mollitia obcaecati
+            dolores doloribus in maiores excepturi, placeat incidunt fugiat ab
+            asperiores molestias quo! Eligendi ipsa hic laudantium harum totam
+            eveniet sit inventore assumenda sed eaque nobis, aliquam, quo
+            laborum iusto. Ea pariatur debitis cupiditate dolor ipsum fugiat
+            voluptatibus. In voluptatibus delectus exercitationem consequatur!
+            Accusantium, officiis quas cumque maiores iure, eligendi tempora et
+            magnam ipsum, qui praesentium velit modi saepe distinctio.
+            Repellendus, ab architecto laborum aspernatur illo delectus autem
+            alias molestiae ratione. Ab, voluptatum? Excepturi eius voluptas,
+            culpa dolor nihil consectetur cumque? Eius, dolores doloribus
+            aliquam sunt libero vitae nobis quis maiores, ad totam blanditiis
+            consectetur dolorem ipsa, voluptatum excepturi praesentium ab eum
+            sit. Consectetur, repellat. Libero laborum labore odio beatae nulla
+            nobis possimus quae maxime nihil. Eaque, quos? Enim unde, harum
+            possimus ipsa omnis eius explicabo nisi non, repellendus vel
+            architecto laudantium voluptatem? Eligendi illum facilis sequi
+            repudiandae, explicabo, hic quaerat molestiae repellat, atque odio
+            itaque molestias exercitationem quae. Fugiat, ullam in asperiores
+            amet maxime quo facere molestias, cum harum praesentium sunt
+            debitis. Repudiandae expedita, necessitatibus nulla quod cupiditate
+            saepe rerum nam! Necessitatibus accusantium doloremque, magni sunt
+            maxime, totam voluptatum dolorum dolor fugiat placeat eveniet sequi
+            nobis in dicta tempore fugit architecto ab?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam deserunt soluta ducimus corporis fuga, aut rem, animi culpa error accusamus quae porro veritatis nihil pariatur hic nulla voluptate tenetur quo.
+            Atque aliquid repudiandae optio totam nam tenetur temporibus. Nostrum delectus esse illo similique nemo minima expedita voluptas maiores necessitatibus. Cumque, laudantium libero iure impedit incidunt sint tempore placeat? Facere, aspernatur.
+            Adipisci, porro fugit architecto nemo nisi esse vel repellat repellendus temporibus tempora eveniet. Consequuntur maxime quae maiores, saepe eaque vero eum eos qui aut suscipit rerum tenetur ullam quas doloremque.
+            Nihil quis repudiandae illum delectus impedit? Qui culpa ipsum corporis modi rem, maxime iusto dignissimos itaque minima eaque laboriosam labore maiores enim minus tempore molestias voluptate tempora et iste nulla!
+            At eveniet provident animi itaque quibusdam doloremque, esse qui officiis accusantium eum ullam modi reiciendis dicta adipisci fugiat odio laborum eaque minima voluptate quam voluptatem? Suscipit soluta fugit animi obcaecati.
+            Sunt culpa eligendi, dolore explicabo vitae asperiores quasi laborum! Molestias molestiae nulla unde, quo maxime velit! Aperiam incidunt saepe hic, aliquam necessitatibus, fuga ut rem iusto, nobis iure repellendus veniam!
+            Hic doloribus dicta odio voluptatibus iste dignissimos sapiente architecto, laudantium aperiam quas vel deserunt facere est. Illum tempore accusantium culpa accusamus! Natus quae, optio corrupti doloribus possimus consectetur velit repellat?
+            Rem, rerum officiis mollitia debitis odit neque inventore facere voluptas enim nostrum dolores officia at nemo laboriosam deserunt omnis? Dolores, nam nobis! Magni amet repellat perspiciatis neque dicta, rem quis.
+            Excepturi commodi quia cumque, dolor architecto minus aperiam recusandae quae explicabo, repellendus eaque animi in nobis ullam voluptatibus quis ipsum velit, maiores nulla repudiandae nesciunt? Illo consequatur laboriosam porro dolorem?
+            Repellat cupiditate, consectetur modi incidunt voluptatum ea non sed iure quis, officiis voluptate molestiae repudiandae perspiciatis asperiores eum accusamus excepturi tempore. Cumque eaque temporibus laborum tempore, id dignissimos molestias voluptatum!
+            Esse amet perferendis quas quibusdam suscipit quisquam doloremque et repellat veniam aperiam, ad voluptates fugit architecto dolorum quo, ipsam ratione aspernatur aut recusandae, maxime quaerat unde. Voluptas a saepe voluptatum?
+            Autem pariatur illo recusandae accusantium optio ipsa omnis aperiam natus eligendi. Repellat pariatur dolor exercitationem hic. Culpa, sunt omnis, dicta, sit aliquam nobis sapiente autem laborum deleniti illo ratione possimus!
+            Aspernatur id expedita, ipsum porro deleniti et laboriosam placeat culpa nulla voluptate consequatur odit magnam cupiditate eum quae aperiam ullam at quod fugiat, temporibus optio. Nihil fuga commodi molestiae mollitia.
+            Quis error blanditiis vel architecto voluptatibus, facilis itaque. Enim aliquam mollitia, minus sit accusantium ipsum error! Inventore debitis itaque ullam assumenda, placeat dicta doloremque illo laborum dolorem? Error, voluptates? Nesciunt.
+            Aliquid
+        </p>
+    </div>
+    <!-- <table>
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>UserID</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each users as { UserID, username }}
+                    <tr>
+                        <td>{username}</td>
+                        <td>{UserID}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table> -->
+</body>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap");
 
-
-
-<h2>
-    hello
-    <Nav/>
-</h2>
+    body {
+        background-color: black;
+        height: 100%;
+    }
+    .hero {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 20vh;
+        background-image: url("/SOPPY_BOBS_dungeon_and_dragons_themed_underground_dungeon_priso_721f4a3b-ab56-4f5c-b52b-9d363c50b32c.png");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        font-size: 25px;
+        padding-bottom: 15%;
+        padding-top:10%;
+    }
+    .hero-text {
+        font-family: "Cinzel Decorative", cursive;
+        color: white;
+    }
+    .hero-image {
+        display: none;
+    }
+    p {
+        color: white;
+    }
+</style>
